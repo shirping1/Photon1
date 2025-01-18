ce14f1a95396db20cd8df7c43f0e9627dcec6355
LeeEunSeok <a4121240@naver.com>
2025-01-18 21:34:52 +0900

    총알 발사 피격 hp바 추가

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviourPun
 {
+    public GameObject bulletPrefab;
+    public Transform firePos;
+    public Image hpBar;
+
     float horizontal;
     float vertical;
+
+    Rigidbody2D rb;
+
+    private float hp;
+
+    private void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+        hp = 100f;
+        hpBar.fillAmount = hp / 100f;
+    }
 
     void Update()
     {
@@ -18,6 +34,49 @@ public class PlayerController : MonoBehaviourPun
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
         Vector3 movement = new Vector3(horizontal, vertical, 0).normalized;
-        transform.position += movement * 5 * Time.deltaTime;
+        transform.position += movement * 3 * Time.deltaTime;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = PhotonNetwork.Instantiate(bulletPrefab.name, firePos.position, firePos.rotation);
+            StartCoroutine(DestoryObject(newBullet, 2f));
+        }
+    }
+
+    IEnumerator DestoryObject(GameObject target, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (tag != null)
+            PhotonNetwork.Destroy(target);
+    }
+
+    [PunRPC]
+    public void GetDamage(float damage)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (hp >= 0)
+            {
+                hp -= damage;
+                photonView.RPC("UpdateHp", RpcTarget.Others, hp);
+                photonView.RPC("GetDamage", RpcTarget.Others, damage);
+            }
+        }
+        hpBar.fillAmount = hp / 100f;
+    }
+
+    [PunRPC]
+    public void UpdateHp(float newHp)
+    {
+        hp = newHp;
     }
 }
---
+++ Assets/Projectile.cs
@@ -0,0 +1,28 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviourPun
+{
+    void Update()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 7);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponentInParent<PlayerController>().GetDamage(10f);
+            
+            PhotonNetwork.Destroy(gameObject);
+        }
+
+    }
+}
